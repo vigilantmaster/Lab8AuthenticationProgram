87b3d0d76bb5381f34b5b0a465fc3f9e413d16d7
Tanel Helmik <vigilantmaster@hotmail.com>
2018-03-10 11:24:17 -0800

    Add project files.

---
+++ Lab8AuthenticationProgram/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab8AuthenticationProgram.Startup))]
+namespace Lab8AuthenticationProgram
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
