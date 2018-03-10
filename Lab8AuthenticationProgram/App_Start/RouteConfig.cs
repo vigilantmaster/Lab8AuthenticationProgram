35ea49dd6758e6415c7c67873a47d0f828cf9554
Tanel Helmik <vigilantmaster@hotmail.com>
2018-03-10 13:04:31 -0800

    can't get to work compilation error

--- Lab8AuthenticationProgram/App_Start/RouteConfig.cs
+++ Lab8AuthenticationProgram/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace Lab8AuthenticationProgram
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "User", action = "List", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
